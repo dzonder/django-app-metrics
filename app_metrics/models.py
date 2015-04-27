be7893412e0a64df05102ea60b218b59f5e94ced
Michal Olech <michal.olech@students.mimuw.edu.pl>
2015-04-27 11:37:46 +0200

    Removed Django 1.5+ compatibility (fixes 'id' bug)

--- app_metrics/models.py
+++ app_metrics/models.py
@@ -1,10 +1,9 @@
 import datetime
 
+from django.contrib.auth import get_user_model
 from django.db import models, IntegrityError
 from django.template.defaultfilters import slugify
 from django.utils.translation import ugettext_lazy as _
-
-from app_metrics.compat import User
 
 
 class Metric(models.Model):
@@ -37,7 +36,7 @@ class MetricSet(models.Model):
     """ A set of metrics that should be sent via email to certain users """
     name = models.CharField(_('name'), max_length=50)
     metrics = models.ManyToManyField(Metric, verbose_name=_('metrics'))
-    email_recipients = models.ManyToManyField(User, verbose_name=_('email recipients'))
+    email_recipients = models.ManyToManyField(get_user_model(), verbose_name=_('email recipients'))
     no_email = models.BooleanField(_('no e-mail'), default=False)
     send_daily = models.BooleanField(_('send daily'), default=True)
     send_weekly = models.BooleanField(_('send weekly'), default=False)
